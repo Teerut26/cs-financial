16a19a3f947afb5b82f3d4d4b0f44350e7b7fd15
Teerut26 <teerutsr@gmail.com>
2022-09-05 00:19:35 +0700

    add : all files

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+import type { NextPage } from 'next'
+
+const Container = styled.div``
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      sdfsd
+    </Container>
+  )
+}
+
+export default Home
